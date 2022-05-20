1dadb4411078189c8453550301791e5276995cf6
prt1996 <prt1996in@gmail.com>
2022-05-20 11:16:03 +0530

    created projct&created header component

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import { render } from "@testing-library/react";
+import React from "react";
+
+export class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1> Librarymanagementsystem </h1>
+      </div>
+    );
+  }
+}
